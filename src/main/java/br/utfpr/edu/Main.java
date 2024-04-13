43d52f2280ddda3f77e531ecc6cbd06a89a68b67
GiovaneWerlang <giovanewerlang@alunos.utfpr.edu.br>
2024-04-13 17:53:32 -0300

    cinza

--- src/main/java/br/utfpr/edu/Main.java
+++ src/main/java/br/utfpr/edu/Main.java
@@ -11,103 +11,87 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        final int MAXSIZE = 600;
-        final int GERAR = 500;
+        //tamanho
+        final int TAMANHO = 600;
+        final byte VALORINICIAL = (byte) 250;
+        final byte VALORDIMINUIR = (byte) 10;
 
-        byte[][] imagem = new byte[MAXSIZE][MAXSIZE];
 
-        //setar cores
-        for (int x = 0; x < MAXSIZE; x++) {
-            for (int y = 0; y < MAXSIZE; y++) {
-                imagem[x][y] = (byte) 255;
+        //criar array 2d de bytes
+        byte[][] imagem = new byte[TAMANHO][TAMANHO];
+
+        //setar cor padrão branca
+        for (int x = 0; x < TAMANHO; x++) {
+            for (int y = 0; y < TAMANHO; y++) {
+                imagem[x][y] = VALORINICIAL;
             }
         }
 
-        //ponto inicial
-        int metade = (MAXSIZE/2) -1;
-        imagem[metade][metade] = (byte) 0;
-
-        int glued = 0;
         Random rand = new Random();
+        //seed baseada no tempo atual
+        rand.setSeed(System.currentTimeMillis());
 
-        while(glued < GERAR){
-            if(glued == GERAR * 0.25 || glued == GERAR * 0.5 || glued == GERAR * 0.75 || glued == GERAR-1) {
-                System.out.println("Gerou: " + glued);
-            }
-            rand.setSeed(System.currentTimeMillis());
-            int startposX = rand.nextInt(0,MAXSIZE-1);
+        int startposX = rand.nextInt(0,TAMANHO-1);
+        int startposY = rand.nextInt(0,TAMANHO-1);
 
-            rand.setSeed(System.currentTimeMillis());
-            int startposY = rand.nextInt(0,MAXSIZE-1);
-            if(imagem[startposX][startposY] != 0){
+        long start = System.currentTimeMillis();
+        int c1 = 0;
+        int c2 = 0;
+        int c3 = 0;
+        int c4 = 0;
 
-                boolean next = false;
-                while(!next) {
+        int dir = 0;
 
+        System.out.println("inicio: " + start);
+        System.out.println("agora: " + System.currentTimeMillis());
+        while((System.currentTimeMillis() - start) < 1000L){
 
-                    rand.setSeed(System.currentTimeMillis());
-                    int dir1 = rand.nextInt(1, 4);
+            boolean next = false;
+            while(!next) {
 
-                    rand.setSeed(System.currentTimeMillis());
-                    int dir2 = rand.nextInt(1, 4);
+                dir = rand.nextInt(1, 5);
 
-                    rand.setSeed(System.currentTimeMillis());
-                    int dir3 = rand.nextInt(1, 4);
-
-                    rand.setSeed(System.currentTimeMillis());
-                    int dir4 = rand.nextInt(1, 4);
-                    int dir = ((dir1 + dir2) + (dir3 + dir4)) / 4;
-
-                    switch (dir) {
-                        case 1: {//Norte
-                            startposX = anterior(startposX, MAXSIZE);
-                            break;
-                        }
-                        case 2: {//Leste
-                            startposY = proximo(startposY, MAXSIZE);
-                            break;
-                        }
-                        case 3: {//Sul
-                            startposX = proximo(startposX, MAXSIZE);
-                            break;
-                        }
-                        case 4: {//Oeste
-                            startposY = anterior(startposY, MAXSIZE);
-                            break;
-                        }
+                switch (dir) {
+                    case 1: {
+                        startposX = anterior(startposX, TAMANHO);
+                        c1++;
+                        break;
                     }
-
-                    if (validaProximidade(startposX, startposY, imagem, MAXSIZE)) {
-                        if(imagem[startposX][startposY] == (byte) 0){
-                            imagem[startposX][startposY] = (byte) ((byte) imagem[startposX][startposY] + (byte) 1);
-                        }else{
-                            imagem[startposX][startposY] = (byte) 0;
-                        }
-                        int count = 0;
-                        for (int x = 0; x < MAXSIZE; x++) {
-                            for (int y = 0; y < MAXSIZE; y++) {
-                                if(imagem[x][y] == 0){
-                                    count++;
-                                }
-                            }
-                        }
-                        glued = count;
-                        next = true;
+                    case 2: {
+                        startposY = proximo(startposY, TAMANHO);
+                        c2++;
+                        break;
+                    }
+                    case 3: {
+                        startposX = proximo(startposX, TAMANHO);
+                        c3++;
+                        break;
+                    }
+                    case 4: {
+                        startposY = anterior(startposY, TAMANHO);
+                        c4++;
+                        break;
                     }
                 }
+
+                imagem[startposX][startposY] -= VALORDIMINUIR;
+                next = true;
+
             }
         }
 
-        //contar
+        //contar pixels gerados
         int count = 0;
-        for (int x = 0; x < MAXSIZE; x++) {
-            for (int y = 0; y < MAXSIZE; y++) {
-                if(imagem[x][y] == 0){
+        for (int x = 0; x < TAMANHO; x++) {
+            for (int y = 0; y < TAMANHO; y++) {
+                if(imagem[x][y] < 255){
                     count++;
                 }
             }
         }
-        System.out.println(count);
+        System.out.println("total gerado: " + count);
+        System.out.println("tempo: " + (System.currentTimeMillis() - start));
+        System.out.println("1: " + c1 + ", 2: " + c2 + ", 3: " + c3 + ", 4: " + c4);
 
         //gerar buffer
         int width = imagem[0].length;
@@ -133,40 +117,12 @@ public class Main {
         imagePlus.show();
     }
 
-    public static boolean validaProximidade(int x, int y, byte[][] imagemProx, int MAXSIZE){
-//        boolean a = imagemProx[proximo(x,MAXSIZE)][y] == 0;
-//        boolean b = imagemProx[anterior(x,MAXSIZE)][y] == 0;
-//        boolean c = imagemProx[x][proximo(y,MAXSIZE)] == 0;
-//        boolean d = imagemProx[x][anterior(y,MAXSIZE)] == 0;
-        boolean a = imagemProx[proximo(x,MAXSIZE)][proximo(y,MAXSIZE)] == (byte) 0;
-        boolean b = imagemProx[anterior(x,MAXSIZE)][anterior(y,MAXSIZE)] == (byte) 0;
-        boolean c = imagemProx[anterior(x,MAXSIZE)][proximo(y,MAXSIZE)] == (byte) 0;
-        boolean d = imagemProx[proximo(x,MAXSIZE)][anterior(y,MAXSIZE)] == (byte) 0;
-//        if(a || b || c || d){
-//            System.out.println("valor: " + imagemProx[x][y] + ", a: " + a + ", b: " + b +", c: " + c +", d: " + d);
-//        }
-        return a || b || c || d;
+    public static int proximo(int p, int TAMANHO){
+        return (p + 1) < TAMANHO ? (p + 1) : 0;
     }
 
-    public static int proximo(int p, int MAXSIZE){
-        return (p + 1) < MAXSIZE ? (p + 1) : 0;
+    public static int anterior(int a, int TAMANHO){
+        return (a - 1) >= 0 ? (a - 1) : TAMANHO-1;
     }
 
-    public static int anterior(int a, int MAXSIZE){
-        return (a - 1) >= 0 ? (a - 1) : (MAXSIZE-1);
-    }
-
-    public static void setarParteInicialLinhaMeio(byte[][] imagem, int tamanho){
-        int metade = (tamanho/2) -1;
-        for (int i = 0; i < tamanho; i++) {
-            imagem[i][metade] = (byte) 0;
-        }
-    }
-
-    public static void setarParteInicialLinhaAbaixo(byte[][] imagem, int tamanho){
-        int metade = (tamanho/2) -1;
-        for (int i = 0; i < tamanho; i++) {
-            imagem[tamanho-1][i] = (byte) 0;
-        }
-    }
 }
